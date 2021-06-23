af9ec29df32ef3d7e521070ab21cdd3731027187
b0829065 <b0829065@cgu.edu.tw>
2021-06-23 12:13:57 +0800

    OOOOOOOO

--- myapi/models.py
+++ myapi/models.py
@@ -10,10 +10,52 @@ class Post(models.Model):
 
 class User(models.Model):
     user_id=models.CharField(max_length=45)
-    Stock_code = models.CharField(max_length=50)
-    content =models.CharField(max_length=20)
+    nickname = models.CharField(max_length=50)
+    password =models.CharField(max_length=20)
 
 class User_login(models.Model): #登入與註冊
     user_id=models.CharField(max_length=45)
-    content =models.CharField(max_length=20)
-    Stock_code = models.CharField(max_length=50)
+    password =models.CharField(max_length=20)
+    nickname = models.CharField(max_length=50)
+    login_check=BooleanField(default=False) 
+
+class Room_detail(models.Model): #留言
+    user_id=models.CharField(max_length=45)
+    nickname = models.CharField(max_length=50)
+    cotent = models.TextField(blank=True)
+    room_title = models.CharField(max_length=45)
+    room_id= models.CharField(max_length=50)
+    date=models.DateTimeField(auto_now_add=True)
+
+class Room(models.Model):
+    room_title=models.CharField(max_length=45)
+    room_id=models.CharField(max_length=50)
+    def __str__(self):
+        return self.room_title
+
+
+class Numbers_room(models.Model): #右側伺服器人員
+    nickname = models.CharField(max_length=50)
+    user_id = models.CharField(max_length=45)
+    room_id = models.CharField(max_length=45)
+    
+
+class Numbers(models.Model):
+    room_id = models.CharField(max_length=45)
+    user_id = models.ForeignKey(User_login, on_delete=models.CASCADE, null=True)
+
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+class Entry(models.Model):
+    blog = models.ForeignKey(Blog ,on_delete=models.CASCADE, null=True)
+    headline = models.CharField(max_length=255)
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=200)
+
+class Food(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+
--- myapi/views.py
+++ myapi/views.py
@@ -14,7 +14,7 @@ from rest_framework import status
 
 import json
 
-from .models import Post, User_login
+from .models import Entry, Food, Numbers, Numbers_room, Post, Restaurant, Room, Room_detail, User_login
 from .models import User
 # Create your views here.
 
@@ -34,18 +34,66 @@ class HelloAPIView(APIView):
 class Add_user(APIView): #註冊
     def get(self,request):
         get_id = request.GET.get('user_id','')
-        get_content = request.GET.get('content','')
-        get_Stock_code = request.GET.get('Stock_code','')
+        get_nickname = request.GET.get('nickname','')
+        get_password = request.GET.get('password','')
+        get_login_check=request.GET.get('login_check','')
         new_user = User_login()
         new_user.user_id = get_id
-        new_user.content = get_content
-        new_user.Stock_code = get_Stock_code
+        new_user.nickname = get_nickname
+        new_user.password = get_password
+        new_user.login_check=get_login_check
         new_user.save()
         if get_id:
             return JsonResponse({'data': get_id + ' insert!'},status=status.HTTP_200_OK)
         else:
             return JsonResponse({'res':'parameter : name is None'},status=status.HTTP_400_BAD_REQUEST)
 
+class Add_Room(APIView): #新增房間
+    def get(self,request):
+        get_id = request.GET.get('room_id','')
+        get_title = request.GET.get('title','')
+        new_user = Room()
+        new_user.room_id = get_id
+        new_user.room_title =get_title 
+        new_user.save()
+        if get_id:
+            return JsonResponse({'data': get_id + '  房間以新增!'},status=status.HTTP_200_OK)
+        else:
+            return JsonResponse({'res':'parameter : name is None'},status=status.HTTP_400_BAD_REQUEST)
+            
+class Addnumbers(APIView): #新增右側人數
+    def get(self,request):
+        get_user_id = request.GET.get('user_id','')
+        get_room_id = request.GET.get('room_id','')
+        get_name=request.GET.get('nickname','')
+        new_user = Numbers_room()
+        new_user.user_id = get_user_id
+        new_user.room_id = get_room_id
+        new_user.nickname = get_name 
+        new_user.save()
+        if get_user_id:
+            return JsonResponse({'ID': get_user_id + '  人員以新增!'},status=status.HTTP_200_OK)
+        else:
+            return JsonResponse({'res':'parameter : name is None'},status=status.HTTP_400_BAD_REQUEST)
+# class Add_Room_detail(APIView): #新增留言 /add_room_detail
+class Add_Room_detail(APIView): #新增留言 /add_room_detail
+    def get(self,request):
+        get_id = request.GET.get('user_id','')
+        get_nickname = request.GET.get('nickname','')
+        get_content = request.GET.get('content','')
+        get_room_title=request.GET.get('room_title','')
+        get_room_id=request.GET.get('room_id','')
+        new_room = Room_detail()
+        new_room.user_id = get_id
+        new_room.nickname = get_nickname
+        new_room.cotent = get_content
+        new_room.room_title=get_room_title
+        new_room.room_id=get_room_id
+        new_room.save()
+        if get_id:
+            return JsonResponse({'data': get_id + ' 留言已新增!'},status=status.HTTP_200_OK)
+        else:
+            return JsonResponse({'res':'parameter : name is None'},status=status.HTTP_400_BAD_REQUEST)
 
 class Delete_user(APIView): #刪除USER /deleteuser
     def get(self,request):
@@ -57,8 +105,35 @@ class Delete_user(APIView): #刪除USER /deleteuser
         else:
             return JsonResponse({'res':'parameter : name is None'},status=status.HTTP_400_BAD_REQUEST)     
 
-
-class Update_user(APIView): #更改帳密 or content/updateuser 
+class Delete_Room_detail(APIView):
+    def get (self,request):
+        get_id=request.GET.get('user_id','')
+        comment=Room_detail.objects.filter(user_id=get_id)
+        comment.delete()
+        if comment.delete():
+            return JsonResponse({'user ID:':get_id + ' 的留言已刪除!'},status=status.HTTP_200_OK)
+        else:
+            return JsonResponse({'res':'parameter : name is None'},status=status.HTTP_400_BAD_REQUEST)
+class Delete_Room_commet(APIView):
+    def get (self,request):
+        get_id=request.GET.get('user_id','')
+        get_content=request.GET.get('content','')
+        comment=Room_detail.objects.filter(user_id=get_id,cotent=get_content)
+        comment.delete()
+        if comment.delete():
+            return JsonResponse({'user ID:':get_id+' 的 '+get_content + ' 留言已刪除!'},status=status.HTTP_200_OK)
+        else:
+            return JsonResponse({'res':'parameter : name is None'},status=status.HTTP_400_BAD_REQUEST)
+class Delete_Numbers(APIView):
+    def get (self,request):
+        get_id=request.GET.get('user_id','')
+        comment=Numbers_room.objects.filter(user_id=get_id)
+        comment.delete()
+        if comment.delete():
+            return JsonResponse({'user ID:':get_id + ' 以踢出房間'},status=status.HTTP_200_OK)
+        else:
+            return JsonResponse({'res':'parameter : name is None'},status=status.HTTP_400_BAD_REQUEST)     
+class Update_user(APIView): #更改帳密 or nickname/updateuser 
     def get(self,request):
         # get_id = request.GET.get('id','')
         get_userid=request.GET.get('user_id','')
@@ -93,7 +168,25 @@ class Login(APIView): #更改登入狀態/login
         else:
             return JsonResponse({'res':'parameter : name is None'},status=status.HTTP_400_BAD_REQUEST)
 
-
+class Room_update(APIView): #更改登入狀態/login
+    def get(self,request):
+        room_id=request.GET.get('room_id','')
+        room_title=request.GET.get('title','')
+        update_user = Room.objects.filter(room_id=room_id)
+        update_user.update(room_title=room_title)
+        title=Room.objects.get(room_id=room_id)
+        for e in Room.objects.all():
+            if(e.room_id==room_id):
+                title=e.room_title
+        print(title)
+        # nickname=User.objects.get(user_id=get_userid)
+        if update_user:
+            if update_user==False:
+                return JsonResponse({'User':room_id + ' 已成功登出'},status=status.HTTP_200_OK)
+            else: 
+                return JsonResponse(title,safe=False)
+        else:
+            return JsonResponse({'res':'parameter : name is None'},status=status.HTTP_400_BAD_REQUEST)
 class List_post(APIView):
     def get(self,request):
         page = request.GET.get('page',1) #  browsing page i
@@ -119,3 +212,44 @@ class List_User(APIView):
             {'data':list(user)},
             status=status.HTTP_200_OK
         )
+class List_Room_detail(APIView):
+    def get (self,request):
+        room = Room_detail.objects.all().values()
+        return JsonResponse(
+         list(room),safe=False   
+        )
+class List_Numbers(APIView):
+    def get(self,request):
+        numbers=Numbers_room.objects.all().values()
+        return JsonResponse(
+            list(numbers),safe=False
+        )
+class List_Room(APIView):
+    def get (self,request):
+        index =request.GET.get('index',None)
+        if index:
+            room=Room.objects.filter(id=index).values()
+        else:
+            room=Room.objects.all().values()
+        return JsonResponse(list(room),safe=False)
+
+class Test(APIView):
+    def get(self,request):
+        restaurants = Restaurant.objects.all().values()  # values()把QuerySet裡的所有Restaurant objects變成dict
+        restaurants = list(restaurants) 
+        return JsonResponse(
+             restaurants,safe=False
+        )
+
+class Add_test(APIView):
+    def get(self,request):
+        rest = request.GET.get('name','')
+        res=Restaurant()
+        res.name=rest
+        res.save()
+        # restaurants = Restaurant.objects.all().values()
+        # res.restaurant=food.name
+        if rest:
+            return JsonResponse({'data': rest + ' insert!'},status=status.HTTP_200_OK)
+        else:
+            return JsonResponse({'res':'parameter : name is None'},status=status.HTTP_400_BAD_REQUEST)
